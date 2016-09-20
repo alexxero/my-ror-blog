25a87150d3e1e6b6b48334ec2b0ff07a4db137d0
alexxero <alexxero@mail.ru>
2016-09-20 17:39:02 +0300

    Initial blog with devise

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+
+  #paperclip
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", small: "35x35", extrasmall: '10x10' }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  # Validate filename
+  validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
+  # Explicitly do not validate
+  do_not_validate_attachment_file_type :avatar
+end
